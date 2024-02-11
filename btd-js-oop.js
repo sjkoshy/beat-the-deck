501a6c05cb935a7a390fd58b5d59177bc23e0c3e
Sherilyn Koshy <sherilyn.koshy@gmail.com>
2024-02-10 23:42:49 -0500

    add pseudocode

---
+++ btd-js-oop.js
@@ -0,0 +1,13 @@
+// Gameplay: Dealer starts with one card face up. Player guesses whether the next card dealt will be higher or lower in value. If the guess is correct, the dealer draws another card, and the player makes another guess. If incorrect, the player loses, and the game ends. The objective is to get as many correct guesses in a row. //
+
+// Create Card //
+
+// Create Deck //
+
+// Initiate Game: Draw One Card //
+
+// Player Guess: Higher or Lower? //
+
+// Draw Another Card // 
+
+// Game End: Display Total Number of Rounds //
